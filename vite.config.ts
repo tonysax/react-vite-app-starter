9d2a12d8949c198840837db33872d7a3d1f6b3bc
Thomas Lamarche <glims@wanadoo.fr>
2024-10-20 12:00:39 +0200

    vitest react test tools add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        // globals: true,
+        environment: 'jsdom',
+        setupFiles: 'setupTests.ts',
+        coverage: {
+            include: ['src/**/*'],
+            exclude: [
+                'src/mocks',
+                'src/vite-env.d.ts',
+                'src/main.tsx',
+                'src/**/*.test.ts',
+            ],
+        },
+    },
 })
