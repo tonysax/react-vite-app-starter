8f1f3fb290e3e04eab767b8327d808749ecee851
Thomas Lamarche <glims@wanadoo.fr>
2024-10-20 13:14:37 +0200

    alias add

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 //import { defineConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        },
+    },
     test: {
         // globals: true,
         environment: 'jsdom',
